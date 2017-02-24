a9ce9ee835ef19ed674485353c43507078878c4a
Noah TerBest <noahterbest@Noahs-iMac.local>
2017-02-23 18:22:28 -0700

    jailbreak files (10.2 for now)

---
+++ yalu102/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  yalu102
+//
+//  Created by qwertyoruiop on 05/01/2017.
+//  Copyright © 2017 kimjongcracks. All rights reserved.
+//
+
+// 悪用の一つになるためには、あなたは反乱軍のものでなければなりません
+// 私はこのコードの何も所有していない
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ yalu102/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  yalu102
+//
+//  Created by qwertyoruiop on 05/01/2017.
+//  Copyright © 2017 kimjongcracks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    IBOutlet UIButton* dope;
+}
+- (IBAction)yolo:(id)sender;
+
+@end
+
+// 悪用の一つになるためには、あなたは反乱軍のものでなければなりません
+// 私はこのコードの何も所有していない
